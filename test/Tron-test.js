7bddf7e9605d7307086bf7bc6ef9b8f70fa5ea14
Daniel Bucket <danieljbucket@gmail.com>
2017-03-22 15:32:00 -0600

    change file names

---
+++ test/Tron-test.js
@@ -0,0 +1,12 @@
+const assert = require('chai').assert;
+const Tron = require('../lib/index.js')
+
+describe('lets test some shit, bruh', () => {
+
+  it(' coordinateArray should be an array', () => {
+    var game = new Tron;
+
+    beginning()
+    assert.isArray(coordinateArray)
+  })
+})
